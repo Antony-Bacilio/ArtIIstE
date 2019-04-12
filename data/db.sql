831294a476578b28820ced34a8d22aeb9f8f5c86
Antony-Bacilio <lucas.herfort@gmail.com>
2019-04-12 07:40:29 -0700

    change db.sql

--- data/db.sql
+++ data/db.sql
@@ -1,19 +1,13 @@
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY ,
-    firstname VARCHAR NOT NULL ,
-    lastname VARCHAR NOT NULL ,
-    birthday date
+    firstname VARCHAR(30),
+    lastname VARCHAR(30),
+    mail VARCHAR NOT NULL,
+    mdp VARCHAR NOT NULL,
+    birthday date,
+    sexe CHAR NOT NULL,
+    art VARCHAR 
 );
 
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('John', 'Doe', '1967-11-22');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Yvette', 'Angel', '1932-01-24');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Amelia', 'Waters', '1981-12-01');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Manuel', 'Holloway', '1979-07-25');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Alonzo', 'Erickson', '1947-11-13');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Otis', 'Roberson', '1995-01-09');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jaime', 'King', '1924-05-30');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Vicky', 'Pearson', '1982-12-12)');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Silvia', 'Mcguire', '1971-03-02');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Brendan', 'Pena', '1950-02-17');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Jackie', 'Cohen', '1967-01-27');
-INSERT INTO "user"(firstname, lastname, birthday) VALUES ('Delores', 'Williamson', '1961-07-19');
+INSERT INTO "user"(firstname, lastname, mail,mdp, birthday, sexe, art) VALUES ('Lucas', 'Herfort', 'lucas.herfort@gmail.com', 'exr29247', '1997-08-22','M','Musique');
+INSERT INTO "user"(firstname, lastname, mail,mdp, birthday, sexe, art) VALUES ('Camille', 'Herfort', 'camille.herfort@gmail.com', 'football33', '2001-06-06','M','Musique');
