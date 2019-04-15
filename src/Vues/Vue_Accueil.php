eb759781eabfde3db88fe506a04cfe951f9f4055
antonybacilio <antonybaciliosalazar@gmail.com>
2019-04-15 15:26:56 +0200

    Ajout Vues

---
+++ src/Vues/Vue_Accueil.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <!--<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap CSS -->
+  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">-->
+  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+  <link rel="stylesheet" href="doc/css/font-awesome.min.css">
+  <link rel="stylesheet" href="doc/css/bootstrap.css">
+  <link rel="stylesheet" href="doc/css/style.css">
+
+  <title>ArtIIstE</title>
+
+  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+
+
+</head>
+
+<body>
+<div class="topbg d-none d-lg-block"></div>
+  <!-- NAV -->
+  <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+          <div class="container">
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <a href="index.php" class="navbar-brand">
+                <img src="doc/img/logo_a.png"  width="40" height="40">
+                <span class="h4 text-success d-inline align-middle mx-2">ArtIIstE</span>
+            </a>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+              <ul class="navbar-nav mr-auto">
+                    <!--<li class="nav-item">
+                      <a class="nav-link" href="index.html">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="client/onglets/a-propos.html">À propos</a>
+                    </li>-->
+              </ul>
+              <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="index.php?page=Connexion">Connexion</a>
+                    </li>
+              </ul>
+            </div>
+          </div>
+  </nav>
+
+    <!--<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>-->
+    <script src="doc/js/jquery.min.js"></script>
+    <script src="doc/js/popper.min.js"></script>
+    <script src="doc/js/bootstrap.min.js"></script>
+    <script src="doc/js/login.js"></script>
+  </body>
+
+</html>
